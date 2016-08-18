1f348bbf5886e6c472563193609a22129f2360bb
wangdegang <1710555800@qq.com>
2016-08-18 15:13:10 +0800

    8/18

--- studyPython/hello.py
+++ studyPython/hello.py
@@ -1,1 +1,11 @@
-name = input()
+# a=100
+# sum = 0
+# while a>0:
+#     sum = sum + a-1
+#     a= a-1
+# print(sum)
+# print(int(12.22))
+# def power(x):
+#     return x*x
+# print(power(122))
+    
